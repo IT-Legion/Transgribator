a9e5c9408daad1cbb485b21669763562fed03ed0
Сергей Перевозников <parik073@gmail.com>
2023-07-28 00:28:14 +0200

    Не большие правки

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 import vosk
 #
 # инициализируем модель распознавания речи
-model = vosk.Model("model_small_ru")
+model = vosk.Model("model/big_model_ru")
+#https://alphacephei.com/vosk/models
 
 # открываем аудиофайл и создаем объект для чтения данных
 filename = "audio.wav"
@@ -14,18 +15,20 @@ wf = wave.open(f"{filename}", "rb")
 # создаем объект распознавания речи
 rec = vosk.KaldiRecognizer(model, wf.getframerate())
 # читаем данные из аудиофайла и передаем их на распознавание
+t=''
 while True:
-    data = wf.readframes(16000)
+    data = wf.readframes(4000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
         result = json.loads(rec.Result())
+        t += result['text']
         print(result["text"])
+print(t)
 
 # получаем окончательный результат распознавания
 
 result = json.loads(rec.FinalResult())
-
 print(result)
 
 
